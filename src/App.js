0b43d8e0c2772a9820836d87f71bba4ddb2f2398
codziller <thekodezilla@gmail.com>
2022-01-14 02:45:39 +0100

    basic UI setup

--- src/App.js
+++ src/App.js
@@ -1,29 +1,26 @@
-import * as React from "react";
-import { ethers } from "ethers";
-import './App.css';
+import * as React from 'react'
+import { ethers } from 'ethers'
+import './App.css'
 
-export default function App() {
-
+export default function App () {
   const wave = () => {
-    
+    alert("Wallet connected and you've waved 👋")
   }
-  
+
   return (
-    <div className="mainContainer">
+    <div className='mainContainer'>
+      <div className='dataContainer'>
+        <div className='header'>👋 Hey there!</div>
 
-      <div className="dataContainer">
-        <div className="header">
-        👋 Hey there!
+        <div className='bio'>
+          I am Thekodezilla and I'm trying to transition from web2 to web3 cool?
+          Connect your Ethereum wallet and wave at me!
         </div>
 
-        <div className="bio">
-        I am farza and I worked on self-driving cars so that's pretty cool right? Connect your Ethereum wallet and wave at me!
-        </div>
-
-        <button className="waveButton" onClick={wave}>
-          Wave at Me
+        <button className='waveButton' onClick={wave}>
+          Connect your wallet to Wave at Me 😉
         </button>
       </div>
     </div>
-  );
+  )
 }
